4322135ee07ddd24ba9b601c146aaca088f074bf
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-11-21 22:59:22 +0700

    Initial commit

---
+++ src/com/rizkykhapidsyah/forbersarang/Main.java
@@ -0,0 +1,13 @@
+package com.rizkykhapidsyah.forbersarang;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
